32b1fa83dfc9da534e51549340619d73a852cb8a
fitrahsyt <fitrahnurull4@gmail.com>
2021-05-25 19:23:37 +0800

    Ganti AppDatabase Baru

--- app/src/main/java/com/fitrahsuyoto/tugasnoteapp/AppDatabase.kt
+++ app/src/main/java/com/fitrahsuyoto/tugasnoteapp/AppDatabase.kt
@@ -1,12 +1,11 @@
 package com.fitrahsuyoto.tugasnoteapp
 
 import android.content.Context
-import android.provider.ContactsContract
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [ContactsContract.CommonDataKinds.Note::class], exportSchema = false, version = 1)
+@Database(entities = [Note::class], exportSchema = false, version = 1)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun noteDao(): NoteDao
 
